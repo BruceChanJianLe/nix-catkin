917e0082c50e647e7174b4d3c1988177ffe7af61
Chan Jian Le <chanjl@i2r.a-star.edu.sg>
2025-04-25 14:42:29 +0800

    wip: update flake
    What a mess!

--- flake.nix
+++ flake.nix
@@ -3,70 +3,131 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
-  let
-    # System types to support
-    supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-
-    # Helper function to generate an attrset
-    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-  in {
-    # Define packages for each system
-    packages = forAllSystems (system:
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        default = pkgs.stdenv.mkDerivation {
-          pname = "nix-catkin";
+        pkgs = import nixpkgs { inherit system; };
+
+        my-catkin = (with pkgs; stdenv.mkDerivation {
+          pname = "my-catkin";
           version = "0.8.11";
+          src = fetchgit {
+            url = "https://github.com/ros/catkin";
+            rev = "0.8.11";
+            sha256 = "4aZb7y24W6WY2dwOj33qf496xp9Sxx8RHvdsyG/OpAA=";
+            fetchSubmodules = true;
+          };
 
-          src = ./.;
-
-          buildInputs = with pkgs; [
-            gcc
-            micromamba
+          nativeBuildInputs = [
+            clang
             cmake
-            git
+            micromamba
+            glibc
+            glibcLocales
           ];
 
           buildPhase = ''
+            eval "$(micromamba shell hook --shell=posix)"
             micromamba activate noetic
-            cmake -S ./catkin -B build
+            cmake -S catkin -B build
             # nothing to build actually
             cmake --build build
           '';
 
           installPhase = ''
-            mkdir -p $out/bin/
-            cp build/bin $out/bin/
+            mkdir -p $out/bin
+            mv build/bin/* $out/bin
           '';
-        };
-      }
-    );
+        }
+      );
+    in rec {
 
-    # Development shells for each system
-    devShells = forAllSystems (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        default = pkgs.mkShell {
-          buildInputs = with pkgs; [
+      defaultApp = flake-utils.lib.mkApp {
+        drv = defaultPackage;
+      };
 
-            # Micromamba for Python environment management
-            micromamba
+      defaultPackage = my-catkin;
 
-            # Build tools
-            gcc
-            cmake
-            git
-
-          ];
-        };
-      }
-    );
-  };
+      devShell = pkgs.mkShell {
+        buildInputs = [
+            pkgs.clang
+            pkgs.cmake
+            pkgs.micromamba
+            pkgs.glibc
+            pkgs.glibcLocales
+        ];
+      };
+    }
+  );
 }
+
+#   in {
+#     # Define packages for each system
+#     packages = forAllSystems (system:
+#       let
+#         pkgs = nixpkgs.legacyPackages.${system};
+#       in
+#       {
+#         default = pkgs.stdenv.mkDerivation {
+#           pname = "nix-catkin";
+#           version = "0.8.11";
+#
+#           src = ./.;
+#
+#           # nativeBuildInputs = [ pkgs.cmake ];
+#
+#           # nativeBuildInputs = with pkgs; [
+#           #   cmake
+#           # ];
+#
+#           buildInputs = with pkgs; [
+#             gcc
+#             micromamba
+#           ];
+#
+#           buildPhase = ''
+#             eval "$(micromamba shell hook --shell=posix)"
+#             # micromamba shell init --shell zsh --root-prefix=~/micromamba
+#             # eval "$(micromamba shell hook --shell bash)"
+#             # micromamba create -n noetic -c conda-forge -f environment.yaml -y
+#             # micromamba activate noetic
+#             cmake -S catkin -B build
+#             # nothing to build actually
+#             cmake --build build
+#           '';
+#
+#           installPhase = ''
+#             mkdir -p $out/bin/
+#             # cp build/bin $out/bin/
+#           '';
+#         };
+#
+#         nativeBuildInputs = [ pkgs.cmake ];
+#       }
+#     );
+#
+#     # Development shells for each system
+#     devShells = forAllSystems (system:
+#       let
+#         pkgs = nixpkgs.legacyPackages.${system};
+#       in
+#       {
+#         default = pkgs.mkShell {
+#           buildInputs = with pkgs; [
+#
+#             # Micromamba for Python environment management
+#             micromamba
+#
+#             # Build tools
+#             gcc
+#             git
+#
+#           ];
+#         };
+#       }
+#     );
+#   };
+# }
